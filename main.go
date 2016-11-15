763ec380d738ebb3ae9506987a37f9c90a409a28
justyn temme <justyntemme@gmail.com>
2016-11-14 23:59:29 -0600

    now we just need to extract the data from the request with regex

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/google/go-github/github"
+)
+
+func main() {
+	fmt.Println("hello")
+
+	client := github.NewClient(nil)
+	orgs, _, err := client.Repositories.List("justyntemme", nil)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(orgs)
+
+}
